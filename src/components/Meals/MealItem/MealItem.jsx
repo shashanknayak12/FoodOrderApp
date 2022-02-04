e09d3eb3e41b13442f97df682796e31eb2eac426
Shashank nayak <shashanknayak.1209@gmail.com>
2022-02-04 16:11:37 +0530

    adding dummy meal items

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './MealItem.module.css'
+
+function MealItem(props) {
+    return (
+        <li className={styles.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={styles.description}>
+                    {props.description}
+                </div>
+                <div className={styles.price}>
+                    {` $${props.price.toFixed(2)}`}
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </li>
+    );
+}
+
+export default MealItem;
