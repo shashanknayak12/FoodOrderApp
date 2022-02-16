000a3893e2f48e5e7896ce1b46d0c0b31eac5b49
Shashank nayak <shashanknayak.1209@gmail.com>
2022-02-16 20:34:47 +0530

    fetching meals via http using firebasse database

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,37 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import styles from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem'
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
 
 
 function AvailableMeals() {
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-http-1ce8c-default-rtdb.firebaseio.com/Meals.json')
+            const responseData = await response.json()
+
+            const loadedMeals = []
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                })
+            }
+            setMeals(loadedMeals)
+        }
+
+        fetchMeals()
+    }, [])
 
 
 
@@ -39,7 +36,7 @@ function AvailableMeals() {
         <section className={styles.meals}>
             <Card >
                 <ul >
-                    {DUMMY_MEALS.map(eachMeal => {
+                    {meals.map(eachMeal => {
                         return (
                             <MealItem
                                 id={eachMeal.id}
